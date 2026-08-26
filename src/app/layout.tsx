a9b73b883e445f1be3785357a5a17817b21ab651
Aasim Nizam <asmsd.407@MacBookAir.lan>
2026-08-26 13:04:13 +0530

    analytics: add Google Tag Manager (GTM-THQH822B) container
    Head loader via Next Script (afterInteractive) + body noscript iframe fallback.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -65,7 +65,29 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={`${serif.variable} ${sans.variable}`}>
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-init" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-THQH822B');`}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
       <body className={`${serif.variable} ${sans.variable} font-sans antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-THQH822B"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
         <WhatsAppButton />
         {/* Google Analytics */}
